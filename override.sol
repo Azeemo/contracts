c3b615906832e756ed2408ad1bacdaf87077f9b3
AtulKumarBisoyi <94751610+AtulKumarBisoyi@users.noreply.github.com>
2023-01-28 19:26:40 +0530

    Create override.sol

---
+++ override.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+import "hardhat/console.sol";
+
+contract contract1
+{
+    function hello() public view virtual
+    {
+        console.log("HELLO from contract 1");
+    }
+}
+
+contract contract2 is contract1
+{
+    function hello() public view override
+    {
+        super.hello();
+        console.log("HELLO from contract 2");
+    }
+}
